d5ba8b9c8e87eb1443d3534fe8e865be6586ba5c
rimi <fla0315@naver.com>
2024-02-04 23:55:11 +0900

    [서버 개발 올인원 패키치] run 시작

--- library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
+++ library-app/src/main/java/com/group/libraryapp/LibraryAppApplication.java
@@ -3,11 +3,12 @@ package com.group.libraryapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // 스프링이 실행될 때 자동으로 설정해주는 어플리케이션
 public class LibraryAppApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(LibraryAppApplication.class, args);
-  }
 
+    SpringApplication.run(LibraryAppApplication.class, args); //스프링 어플리케이션을 실행한다
+
+  }
 }
